69b217ec70477cd2037d7ec5b8997b78293cf28b
tangweichang <tangweichangjlu@gmail.com>
2015-11-20 23:54:23 -0500

    add POST method

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [];
+var todoNextId = 1;
 
-var todos = [{
-    id: 1,
-    description: 'Meet mom for lunch',
-    completed: false
-}, {
-    id: 2,
-    description: 'Go to market',
-    completed: false
-}, {
-    id: 3,
-    description: 'Play computer games',
-    completed: false
-}];
+app.use(bodyParser.json());
+
+
+
+//var todos = [{
+//    id: 1,
+//    description: 'Meet ben for lunch',
+//    completed: false
+//}, {
+//    id: 2,
+//    description: 'Go to market',
+//    completed: false
+//}, {
+//    id: 3,
+//    description: 'Play computer games',
+//    completed: false
+//}];
 
 app.get('/', function(req,res) {
     res.send('Todo API Root');
@@ -41,10 +49,20 @@ app.get('/todos/:id', function(req,res) {
         res.status(404).send();   
     }
     // Iterate of todos array. Find the match.
-    
 
-    
 });
+
+//POST /todos/:id
+app.post('/todos', function(req,res) {
+    var body = req.body;
+    body.id = todoNextId;
+    todoNextId++;
+    todos.push(body);
+    console.log('description ' + body.description);
+    res.json(body);
+});
+
+
 
 app.listen(PORT, function() {
     console.log('Express listening on port ' + PORT + '!');
