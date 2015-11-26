84051975798718cf44dcd2a2fb47e067206adcfc
tangweichang <tangweichangjlu@gmail.com>
2015-11-26 01:58:55 -0500

    hash and salt password

--- models/user.js
+++ models/user.js
@@ -1,3 +1,6 @@
+var bcrypt = require('bcrypt');
+var _ = require('underscore');
+
 module.exports = function (sequlize, DataTypes) {
     return sequlize.define('user', {
         email: {
@@ -8,11 +11,24 @@ module.exports = function (sequlize, DataTypes) {
                 isEmail: true
             }
         },
+        salt: {
+            type: DataTypes.STRING  
+        },
+        password_hash: {
+            type: DataTypes.STRING    
+        },
         password: {
-            type: DataTypes.STRING,
+            type: DataTypes.VIRTUAL,
             allowNull: false,
             validate: {
                 len: [6, 100]    
+            },
+            set: function(value) {
+                var salt = bcrypt.genSaltSync(10);
+                var hashedPassword = bcrypt.hashSync(value, salt);
+                this.setDataValue('password',value);
+                this.setDataValue('salt', salt);
+                this.setDataValue('password_hash', hashedPassword);
             }
         }
     }, {
@@ -21,6 +37,12 @@ module.exports = function (sequlize, DataTypes) {
                 if (typeof user.email === 'string') {
                     user.email = user.email.toLowerCase();    
                 }
+            }
+        },
+        instanceMethods: {
+            toPublicJSON: function() {
+                var json = this.toJSON();
+                return _.pick(json, 'id', 'email', 'updatedAt');
             }
         }
     });
