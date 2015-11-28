b9067d080c522f5393ecb13715ae38d445e964ae
tangweichang <tangweichangjlu@gmail.com>
2015-11-28 18:56:08 -0500

    Add auth middleware and findByToken class method

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
 var bcrypt = require('bcrypt');
+var middleware = require('./middleware.js')(db);
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -32,7 +33,7 @@ app.get('/', function(req,res) {
 });
 
 // GET /todos?completed=true
-app.get('/todos', function(req,res) {
+app.get('/todos',middleware.requireAuthentication, function(req,res) {
     var query = req.query;
     var where = {};
     
@@ -74,7 +75,7 @@ app.get('/todos', function(req,res) {
 });
 
 // GET /todos/:id
-app.get('/todos/:id', function(req,res) {
+app.get('/todos/:id',middleware.requireAuthentication, function(req,res) {
     var todoId = parseInt(req.params.id, 10);
     
     db.todo.findById(todoId).then(function(todo) {
@@ -106,7 +107,7 @@ app.get('/todos/:id', function(req,res) {
 });
 
 //POST /todos/:id
-app.post('/todos', function(req,res) {
+app.post('/todos',middleware.requireAuthentication, function(req,res) {
     var body = _.pick(req.body, 'description', 'completed');
     
     db.todo.create(body).then(function (todo) {
@@ -129,7 +130,7 @@ app.post('/todos', function(req,res) {
 });
 
 
-app.delete('/todos/:id', function(req,res) {
+app.delete('/todos/:id',middleware.requireAuthentication, function(req,res) {
     var todoId = parseInt(req.params.id, 10);
     
     db.todo.destroy({
@@ -161,7 +162,7 @@ app.delete('/todos/:id', function(req,res) {
 });
 
 // PUT /todos/:id
-app.put('/todos/:id', function(req, res) {
+app.put('/todos/:id',middleware.requireAuthentication, function(req, res) {
     var todoId = parseInt(req.params.id, 10);
     var body = _.pick(req.body, 'description', 'completed');
     var attributes = {};
