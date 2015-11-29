59d7f5459d0c1376b7708dcfffea3b99a408ce53
tangweichang <tangweichangjlu@gmail.com>
2015-11-28 22:36:11 -0500

    Convert CRUD request to use current todos only

--- server.js
+++ server.js
@@ -35,7 +35,9 @@ app.get('/', function(req,res) {
 // GET /todos?completed=true
 app.get('/todos',middleware.requireAuthentication, function(req,res) {
     var query = req.query;
-    var where = {};
+    var where = {
+        userId: req.user.get('id')
+    };
     
     if (query.hasOwnProperty('completed') && query.completed === 'true') {
         where.completed = true;   
@@ -78,7 +80,13 @@ app.get('/todos',middleware.requireAuthentication, function(req,res) {
 app.get('/todos/:id',middleware.requireAuthentication, function(req,res) {
     var todoId = parseInt(req.params.id, 10);
     
-    db.todo.findById(todoId).then(function(todo) {
+    db.todo.findOne({
+        where: {
+            id: todoId,
+            userId: req.user.get('id')
+        }
+        
+    }).then(function(todo) {
         if (!!todo) {
             res.json(todo.toJSON());
         } else {
@@ -142,7 +150,8 @@ app.delete('/todos/:id',middleware.requireAuthentication, function(req,res) {
     
     db.todo.destroy({
         where: {
-            id: todoId   
+            id: todoId,
+            userId: req.user.get('id')
         }
     }).then(function(rowsDeleted){
         if (rowsDeleted === 0) {
@@ -182,7 +191,11 @@ app.put('/todos/:id',middleware.requireAuthentication, function(req, res) {
         
     }
     
-    db.todo.findById(todoId).then(function (todo) {
+    db.todo.findOne({
+        where: todoId,
+        userId: req.user.get('id')
+        
+    }).then(function (todo) {
         if (todo) {
             todo.update(attributes).then( function(todo){
                 res.json(todo.toJSON());
@@ -224,7 +237,7 @@ app.post('/users/login', function(req, res) {
 
 
 db.sequelize.sync({
-//    force: true
+    force: true
 }).then(function() {
     app.listen(PORT, function() {
         console.log('Express listening on port ' + PORT + '!');   
